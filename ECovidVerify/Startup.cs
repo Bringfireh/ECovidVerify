912679cc2e781d7c9654e556bf120e18575129ae
Bringfireh <ngutortsenongo@gmail.com>
2022-01-28 00:09:47 +0100

    Add project files.

---
+++ ECovidVerify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECovidVerify.Startup))]
+namespace ECovidVerify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
