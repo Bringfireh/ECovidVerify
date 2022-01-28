47e064f07e704a94fac4d9d385088d927ab33489
Bringfireh <ngutortsenongo@gmail.com>
2022-01-28 02:36:50 +0100

    added answer controller and views

---
+++ ECovidVerify/Controllers/AnswersController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ECovidVerify.Models;
+
+namespace ECovidVerify.Controllers
+{
+    public class AnswersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Answers
+        public ActionResult Index()
+        {
+            var answer = db.Answer.Include(a => a.PatientInfo).Include(a => a.Question);
+            return View(answer.ToList());
+        }
+
+        // GET: Answers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Answer answer = db.Answer.Find(id);
+            if (answer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(answer);
+        }
+
+        // GET: Answers/Create
+        public ActionResult Create()
+        {
+            ViewBag.PatientInfoId = new SelectList(db.PatientInfo, "Id", "FirstName");
+            ViewBag.QuestionId = new SelectList(db.Question, "Id", "QuestionText");
+            return View();
+        }
+
+        // POST: Answers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,QuestionId,PatientInfoId")] Answer answer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Answer.Add(answer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PatientInfoId = new SelectList(db.PatientInfo, "Id", "FirstName", answer.PatientInfoId);
+            ViewBag.QuestionId = new SelectList(db.Question, "Id", "QuestionText", answer.QuestionId);
+            return View(answer);
+        }
+
+        // GET: Answers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Answer answer = db.Answer.Find(id);
+            if (answer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PatientInfoId = new SelectList(db.PatientInfo, "Id", "FirstName", answer.PatientInfoId);
+            ViewBag.QuestionId = new SelectList(db.Question, "Id", "QuestionText", answer.QuestionId);
+            return View(answer);
+        }
+
+        // POST: Answers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,QuestionId,PatientInfoId")] Answer answer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(answer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PatientInfoId = new SelectList(db.PatientInfo, "Id", "FirstName", answer.PatientInfoId);
+            ViewBag.QuestionId = new SelectList(db.Question, "Id", "QuestionText", answer.QuestionId);
+            return View(answer);
+        }
+
+        // GET: Answers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Answer answer = db.Answer.Find(id);
+            if (answer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(answer);
+        }
+
+        // POST: Answers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Answer answer = db.Answer.Find(id);
+            db.Answer.Remove(answer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
